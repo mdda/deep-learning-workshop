65019f198d5faff81de21235d2e29e680bfea1b5
Martin Andrews <GitHub@mdda.net>
2016-02-21 01:45:48 +0800

    Separate out commands for different parts of building

--- vm-inside/configure-vm.bash
+++ vm-inside/configure-vm.bash
@@ -1,16 +1,16 @@
 #!/bin/bash -
 
-# This runs inside the throwaway guest at first boot.
+# This runs inside the guest at first boot.
 
 set -e
 set -x
 
 cd /home/user
-#source config
+source configure-vm.conf
 
 # Build from SRPM.
 #rpmbuild --define '_topdir /home/build' --rebuild /home/build/$srpm
 
-# If we get this far, everything built successfully.
+# If we get this far, everything installed/initialised successfully.
 # This string is detected in the guest afterwards.
-echo '=== BUILD FINISHED OK ==='
+echo '=== CONFIGURATION FINISHED OK ==='
