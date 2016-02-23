fca106c9324920c3d63479d1fc60551d4a22f5ea
Martin Andrews <GitHub@mdda.net>
2016-02-23 23:29:36 +0800

    Python pip install

--- vm-inside/configure-vm.bash
+++ vm-inside/configure-vm.bash
@@ -11,6 +11,11 @@ source configure-vm.conf
 # Build from SRPM.
 #rpmbuild --define '_topdir /home/build' --rebuild /home/build/$srpm
 
+virtualenv --system-site-packages env
+. env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt 
+
 # If we get this far, everything installed/initialised successfully.
 # This string is detected in the guest afterwards.
 echo '=== CONFIGURATION FINISHED OK ==='
