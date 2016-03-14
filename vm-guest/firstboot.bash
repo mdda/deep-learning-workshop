e0121e4d975392e402725ae7a2ae8a5f337518c0
Martin Andrews <GitHub@mdda.net>
2016-03-14 14:08:24 +0800

    Expand requirements to include theano and lasagne 'master'

--- vm-guest/firstboot.bash
+++ vm-guest/firstboot.bash
@@ -10,15 +10,19 @@ source ./config/params
 
 virtualenv --system-site-packages env
 . env/bin/activate
+
 pip install --upgrade pip
-pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
+#pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 
 pip install -r ./config/requirements.txt 
+
 # Once this ~/env is built, we can copy it out to the host machine for 'caching'
 
-# Create this directories safely
+
+# Create these directories safely, just so we know they're there
 mkdir -p $notebook_dir
 mkdir -p $tensorflow_dir
+
 
 # If we get this far, everything installed/initialised successfully.
 # This string is detected in the guest afterwards.
