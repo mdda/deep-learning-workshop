0c102c7780b6e24f61fbae8f226c339637e16857
Martin Andrews <GitHub@mdda.net>
2016-03-15 21:47:03 +0800

    Moving models and data around a little

--- 2-get-data-and-models.bash
+++ 2-get-data-and-models.bash
@@ -12,6 +12,19 @@ source ./vm-config/params
 
 
 ## VGG16 model (converted from Caffee, importable into Keras)
+mkdir -p ./notebooks/data
+pushd ./notebooks/data
+
+## thousand lines with synset IDs and names 
+if [ ! -e "imagenet_synset_words.txt" ]; then
+  wget http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
+  gunzip caffe_ilsvrc12.tar.gz 
+  tar -xf caffe_ilsvrc12.tar synset_words.txt
+  mv synset_words.txt imagenet_synset_words.txt
+  rm caffe_ilsvrc12.tar
+fi
+
+popd
 
 mkdir -p ./notebooks/data/VGG16
 pushd ./notebooks/data/VGG16
@@ -32,9 +45,6 @@ if [ ! -e "vgg16_weights.h5" ]; then
   echo "and save it in './notebooks/data/VGG16/vgg16_weights.h5' yourself"
   echo "See link at : https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3"
 fi
-
-
-
 
 popd
 
