00ab9d5861cecb2dea9b32a4f450bd0155fa0784
Martin Andrews <GitHub@mdda.net>
2016-03-19 01:45:39 +0800

    Art-Style seems to work (at least a little)

--- notebooks/model/googlenet.py
+++ notebooks/model/googlenet.py
@@ -41,14 +41,14 @@ def build_inception_module(name, input_layer, nfilters):
 def build_model():
     net = {}
     net['input'] = InputLayer((None, 3, None, None))
-    net['conv1/7x7_s2'] = ConvLayer(net['input'], 64, 7, stride=2, pad=3)     ## Style?
+    net['conv1/7x7_s2'] = ConvLayer(net['input'], 64, 7, stride=2, pad=3)     ## Style?  conv1/7x7_s2
     net['pool1/3x3_s2'] = PoolLayer(net['conv1/7x7_s2'],
                                     pool_size=3,
                                     stride=2,
                                     ignore_border=False)
     net['pool1/norm1'] = LRNLayer(net['pool1/3x3_s2'], alpha=0.00002, k=1)
     net['conv2/3x3_reduce'] = ConvLayer(net['pool1/norm1'], 64, 1)
-    net['conv2/3x3'] = ConvLayer(net['conv2/3x3_reduce'], 192, 3, pad=1)      ## Style?
+    net['conv2/3x3'] = ConvLayer(net['conv2/3x3_reduce'], 192, 3, pad=1)      ## Style?  conv2/3x3
     net['conv2/norm2'] = LRNLayer(net['conv2/3x3'], alpha=0.00002, k=1)
     net['pool2/3x3_s2'] = PoolLayer(net['conv2/norm2'], pool_size=3, stride=2)
 
@@ -58,7 +58,7 @@ def build_model():
     net.update(build_inception_module('inception_3b',
                                       net['inception_3a/output'],
                                       [64, 128, 128, 192, 32, 96]))
-    net['pool3/3x3_s2'] = PoolLayer(net['inception_3b/output'],               ## Style?
+    net['pool3/3x3_s2'] = PoolLayer(net['inception_3b/output'],               ## Style?  inception_3b/output
                                     pool_size=3, stride=2)
 
     net.update(build_inception_module('inception_4a',
@@ -68,15 +68,15 @@ def build_model():
                                       net['inception_4a/output'],
                                       [64, 160, 112, 224, 24, 64]))
     net.update(build_inception_module('inception_4c',
-                                      net['inception_4b/output'],
+                                      net['inception_4b/output'],             ## Content?  inception_4b/output
                                       [64, 128, 128, 256, 24, 64]))
     net.update(build_inception_module('inception_4d',
-                                      net['inception_4c/output'],
+                                      net['inception_4c/output'],       
                                       [64, 112, 144, 288, 32, 64]))
     net.update(build_inception_module('inception_4e',
-                                      net['inception_4d/output'],
+                                      net['inception_4d/output'],             ## Style? == inception_4d/output ? 
                                       [128, 256, 160, 320, 32, 128]))
-    net['pool4/3x3_s2'] = PoolLayer(net['inception_4e/output'],               ## Content?
+    net['pool4/3x3_s2'] = PoolLayer(net['inception_4e/output'],               
                                     pool_size=3, stride=2)
 
     net.update(build_inception_module('inception_5a',
@@ -86,7 +86,7 @@ def build_model():
                                       net['inception_5a/output'],
                                       [128, 384, 192, 384, 48, 128]))
 
-    net['pool5/7x7_s1'] = GlobalPoolLayer(net['inception_5b/output'])         ## Style? == inception_5b/output ?
+    net['pool5/7x7_s1'] = GlobalPoolLayer(net['inception_5b/output'])         
     net['loss3/classifier'] = DenseLayer(net['pool5/7x7_s1'],
                                          num_units=1000,
                                          nonlinearity=linear)
