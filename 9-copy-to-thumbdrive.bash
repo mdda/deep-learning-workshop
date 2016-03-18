5d29e579f8a7f40fde352248d1a40097b19abd01
Martin Andrews <GitHub@mdda.net>
2016-03-18 11:19:08 +0800

    Clean up ova selection for rsync

--- 9-copy-to-thumbdrive.bash
+++ 9-copy-to-thumbdrive.bash
@@ -9,8 +9,18 @@ fi
 echo "Target: '${target}/'"
 #exit 1
 
+most_recent_ova=`ls ./vm-images/*.ova | tail -1`
+if [ -z "$most_recent_ova" ]; then
+  echo "Need to have created a .ova file first"
+  exit 1
+fi
+
 # target ~ /run/media/andrewsm/591F-4AF0
 
-rsync -avz --progress ./vm-images/fossasia-mdda_*.ova target/
+rsync -avz --progress ${most_recent_ova} target/
+
+# Windows and OSX binaries
 rsync -avz --progress ./vm-images/VirtualBox-5* target/
+
+# Linux binaries (Fedora, and some Ubuntus)
 rsync -avz --progress ./vm-images/virtualbox-5* target/
