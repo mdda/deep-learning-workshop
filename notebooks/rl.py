f5f274d3791e45c7cab8be50aad2672c669ea4e0
Martin Andrews <GitHub@mdda.net>
2016-06-15 22:31:14 +0800

    Stub for make_features()

--- notebooks/rl.py
+++ notebooks/rl.py
@@ -1,4 +1,17 @@
+import numpy as np
+
 from game import crush
 
+#print("Hello from rl.py")
 
-print("Hello from rl.py")
+
+def make_features(board):
+  return board.reshape((-1,))
+
+
+np.random.seed(1)
+
+n_colours = 5
+b = crush.new_board(10,14,n_colours) # Same as portrait phone  1 screen~1k,  high-score~14k
+
+print( make_features(b) )
