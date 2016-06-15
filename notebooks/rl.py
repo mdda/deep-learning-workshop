9a56396b475c3d54a49a63f96ed94ccddf8499bb
Martin Andrews <GitHub@mdda.net>
2016-06-16 00:16:15 +0800

    Efficient board comparisons with shifts

--- notebooks/rl.py
+++ notebooks/rl.py
@@ -6,7 +6,54 @@ from game import crush
 
 
 def make_features(board):
-  return board.reshape((-1,))
+  features = []
+  #print(board)
+  #print( np.pad(board, ((0,0),(1,0)), mode='constant') )
+  
+  #shifted_up = np.pad(board, ((0,0),(1,0)), mode='constant')[:,:-1]
+  #print(shifted_up)
+  #
+  #sameness = np.equal( board[:,:], shifted_up )
+  #print(sameness * 1)
+  
+  #print("Board mask")
+  mask     = np.greater( board[:, :], 0 )
+  #print(mask * 1)
+  features.append( mask.reshape((-1,)) )
+  
+  # This works out whether each cell is the same as the cell 'above it'
+  for shift_up in range(1,4):
+    #print("\n'UP' by %d:" % (shift_up,))
+    # Actually, no need for np.pad, just choose the views appropriately
+    sameness = np.equal(   board[:, :-shift_up], board[:, shift_up:] )
+    print(sameness * 1)
+    
+    mask     = np.greater( board[:, :-shift_up], 0 )
+    print(mask * 1)
+    
+    print(np.logical_and(sameness, mask) * 1)
+    
+    features.append( np.logical_and(sameness, mask).reshape((-1,)) )
+  
+  
+  #shifted_left = np.pad(board, ((1,0),(0,0)), mode='constant')[:-2,:]
+  #print(shifted_left)
+  
+  # This works out whether each cell is the same as the cell in to columns 'to the left of it'
+  for shift_left in range(1,3):
+    print("\n'LEFT' by %d:" % (shift_left,))
+    sameness = np.equal(   board[:-shift_left, :], board[shift_left:, :] )
+    print(sameness * 1)
+    
+    mask     = np.greater( board[:-shift_left, :], 0 )
+    print(mask * 1)
+    
+    print(np.logical_and(sameness, mask) * 1)
+  
+    features.append( np.logical_and(sameness, mask).reshape((-1,)) )
+  
+  #return board.reshape((-1,))
+  return np.concatenate(features)
 
 
 np.random.seed(1)
