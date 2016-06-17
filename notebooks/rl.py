957ac022023333f77e21d3d054cd5ae5adedcfd9
Martin Andrews <GitHub@mdda.net>
2016-06-17 22:50:58 +0800

    aggregate stats for 100 games (played randomly)

--- notebooks/rl.py
+++ notebooks/rl.py
@@ -145,7 +145,7 @@ def play_game(game_id, model):
     
     (h,v) = moves[i]
     print("Move : (%d,%d)" % (h,v))
-    crush.show_board(board, highlight=(h,v))
+    #crush.show_board(board, highlight=(h,v))
     
     #board, score = crush.after_move(board, h,v, -1)
     board, score = crush.after_move(board, h,v, n_colours)
@@ -153,7 +153,7 @@ def play_game(game_id, model):
     score_total += score
     
     print("  score : %d " % (score,))
-    crush.show_board(board, highlight=(0,0))
+    #crush.show_board(board, highlight=(0,0))
     
     game_step += 1
     
@@ -162,13 +162,27 @@ def play_game(game_id, model):
 
 
 model=None
-for i in range(0, 10):
+stats_log=[]
+for i in range(0, 100):
   stats, training_data = play_game(i, model)
   
   print("steps = %d" % (stats['steps'],))
-  print("average moves = %5.1f" % ( stats['av_potential_moves'], ) )
+  print("average moves = %5.1f" % (stats['av_potential_moves'], ) )
   print("score_total = %d" % (stats['score'],))
   
+  stats_log.append( stats )
 
 print("DONE")
+
+stats_cols = "steps av_potential_moves score".split()
+stats_overall = np.array([ [s[c] for c in stats_cols] for s in stats_log ])
+
+print("Min  : ",zip(stats_cols, np.min(stats_overall, axis=0).tolist()) )
+print("Max  : ",zip(stats_cols, np.max(stats_overall, axis=0).tolist()) )
+print("Mean : ",zip(stats_cols, np.mean(stats_overall, axis=0).tolist()) )
+
+# Aggregate stats for 100 games (played randomly)
+#('Min  : ', [('steps', 29.0), ('av_potential_moves', 7.898550724637682), ('score', 246.0)])
+#('Max  : ', [('steps', 118.0), ('av_potential_moves', 16.0), ('score', 1152.0)])
+#('Mean : ', [('steps', 52.68), ('av_potential_moves', 11.719583093032318), ('score', 451.58)])
 
