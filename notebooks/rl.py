89519759e2c23a3ec971e3156a7868b749669542
Martin Andrews <GitHub@mdda.net>
2016-06-18 23:31:58 +0800

    Now trying to learn (with time-to-end estimation)

--- notebooks/rl.py
+++ notebooks/rl.py
@@ -67,7 +67,7 @@ def make_features_in_layers(board):
   feature_layers.append( mask.astype('float32') )
   
   # This works out whether each cell is the same as the cell 'above it'
-  for shift_down in [1,2,3,]:
+  for shift_down in [1,2,3,4,5,]:
     #print("\n'DOWN' by %d:" % (shift_down,))
     sameness = np.zeros_like(board, dtype='float32')
     
@@ -77,7 +77,7 @@ def make_features_in_layers(board):
     feature_layers.append( sameness )
   
   # This works out whether each cell is the same as the cell in to columns 'to the left of it'
-  for shift_right in [1,2,]:
+  for shift_right in [1,2,3,]:
     #print("\n'RIGHT' by %d:" % (shift_right,))
     sameness = np.zeros_like(board, dtype='float32')
     
@@ -229,9 +229,7 @@ def play_game(game_id, model):
     score_total += score
     new_cols_total += new_cols
     
-    print("Move[%2d]=(%2d,%2d) -> Score : %3d, new_cols=%1d" % (i, h,v, score,new_cols))
-    #exit(0)
-    #print("  score : %d " % (score,))
+    #print("Move[%2d]=(%2d,%2d) -> Score : %3d, new_cols=%1d" % (i, h,v, score,new_cols))
     #crush.show_board(board, highlight=(0,0))
 
     game_step += 1
@@ -249,48 +247,63 @@ model=build_cnn(board_input, features_shape)
 predict_q_value  = lasagne.layers.get_output(model, deterministic=True)
 estimate_q_value = lasagne.layers.get_output(model)
 
-model_squared_error = lasagne.objectives.squared_error(estimate_q_value, board_score).mean()
-model_squared_error.name = "MSE"
+model_squared_error = lasagne.objectives.squared_error(estimate_q_value.reshape( (-1,) ), board_score).mean()
 
 model_params  = lasagne.layers.get_all_params(model, trainable=True)
 #model_updates = lasagne.updates.nesterov_momentum( model_squared_error, model_params, learning_rate=0.01, momentum=0.9 )
 model_updates = lasagne.updates.adam( model_squared_error, model_params )
 
 model_evaluate_features = theano.function([board_input], predict_q_value)
-model_train             = theano.function([board_input, board_score], [model_squared_error], updates=model_updates)
+model_train             = theano.function([board_input, board_score], model_squared_error, updates=model_updates)
 
-  
+import datetime
+t0 = datetime.datetime.now()
+
+n_games=1000
 stats_log=[]
-for i in range(0, 2):
+for i in range(0, n_games):
   stats, training_data = play_game(i, model)
   
-  print("steps = %d" % (stats['steps'],))
-  print("average moves = %5.1f" % (stats['av_potential_moves'], ) )
-  print("new_cols = %d" % (stats['new_cols'],))
-  print("score_total = %d" % (stats['score'],))
+  print("game[%d]" % (i,))
+  print("  steps         = %d" % (stats['steps'],))
+  print("  average moves = %5.1f" % (stats['av_potential_moves'], ) )
+  print("  new_cols      = %d" % (stats['new_cols'],))
+  print("  score_total   = %d" % (stats['score'],))
   
-  print( np.asarray( training_data['board'] ).shape )
-  print( np.asarray( training_data['score'] ).reshape( (1,-1) ).shape )
+  #print( np.asarray( training_data['board'] ).shape )
+  #print( np.asarray( training_data['score'] ).reshape( (1,-1) ).shape )
   
-  #err = model_train( np.array(training_data['board']), np.array(training_data['score'], dtype='float32') )
-  #err = model_train( training_data['board'], np.array(training_data['score'], dtype='float32')) # .reshape( (1,-1) ) 
   err = model_train( training_data['board'], training_data['score'] )
   
   stats['model_err'] = err
   
   stats_log.append( stats )
-
-print("DONE")
+  
+  if ((i+1) % 10)==0:
+    t_now = datetime.datetime.now()
+    t_elapsed = (t_now - t0).total_seconds()
+    t_end_projected = t0 + datetime.timedelta( seconds=n_games* (t_elapsed/i) )
+    print("    100 games in %6.1f seconds, Projected end at : %s" % (100.*t_elapsed/i, t_end_projected.strftime("%H:%M"),))
+    
 
 stats_cols = "steps av_potential_moves new_cols score model_err".split()
 stats_overall = np.array([ [s[c] for c in stats_cols] for s in stats_log ])
 
-print("Min  : ",zip(stats_cols, np.min(stats_overall, axis=0).tolist()) )
-print("Max  : ",zip(stats_cols, np.max(stats_overall, axis=0).tolist()) )
-print("Mean : ",zip(stats_cols, np.mean(stats_overall, axis=0).tolist()) )
+print("Min  : ",zip(stats_cols, ["%6.1f" % (v,) for v in np.min(stats_overall, axis=0).tolist()]) )
+print("Max  : ",zip(stats_cols, ["%6.1f" % (v,) for v in np.max(stats_overall, axis=0).tolist()]) )
+print("Mean : ",zip(stats_cols, ["%6.1f" % (v,) for v in np.mean(stats_overall, axis=0).tolist()]) )
 
 # Aggregate stats for 100 games (played randomly)
-#('Min  : ', [('steps', 29.0), ('av_potential_moves', 7.898550724637682), ('score', 246.0)])
-#('Max  : ', [('steps', 118.0), ('av_potential_moves', 16.0), ('score', 1152.0)])
-#('Mean : ', [('steps', 52.68), ('av_potential_moves', 11.719583093032318), ('score', 451.58)])
+#('Min  : ', [('steps', '  29.0'), ('av_potential_moves', '   7.9'), ('new_cols', '   0.0'), ('score', ' 246.0'), ('model_err', '  45.9')])
+#('Max  : ', [('steps', ' 118.0'), ('av_potential_moves', '  16.0'), ('new_cols', '  18.0'), ('score', '1152.0'), ('model_err', '7681.9')])
+#('Mean : ', [('steps', '  52.7'), ('av_potential_moves', '  11.7'), ('new_cols', '   3.0'), ('score', ' 451.6'), ('model_err', '1235.0')])
 
+# Aggregate stats for 100 games (played with learning : ADAM per game)
+#('Min  : ', [('steps', '  25.0'), ('av_potential_moves', '   6.6'), ('new_cols', '   0.0'), ('score', ' 266.0'), ('model_err', '  41.0')])
+#('Max  : ', [('steps', '  98.0'), ('av_potential_moves', '  17.9'), ('new_cols', '  13.0'), ('score', ' 880.0'), ('model_err', '8107.7')])
+#('Mean : ', [('steps', '  53.0'), ('av_potential_moves', '  11.1'), ('new_cols', '   3.4'), ('score', ' 451.6'), ('model_err', '1284.6')])
+
+# Aggregate stats for 1000 games (played with learning : ADAM per game)
+#('Min  : ', [('steps', '  26.0'), ('av_potential_moves', '   6.1'), ('new_cols', '   0.0'), ('score', ' 212.0'), ('model_err', '   7.8')])
+#('Max  : ', [('steps', ' 139.0'), ('av_potential_moves', '  19.9'), ('new_cols', '  21.0'), ('score', '1150.0'), ('model_err', '15163.5')])
+#('Mean : ', [('steps', '  54.1'), ('av_potential_moves', '  12.2'), ('new_cols', '   3.4'), ('score', ' 469.0'), ('model_err', ' 221.5')])
