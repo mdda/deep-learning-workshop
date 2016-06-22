5b7dfb749d26d199150551c9c2d2b3504f851dc2
Martin Andrews <GitHub@mdda.net>
2016-06-22 22:51:47 +0800

    Bubble Breaker has scores

--- notebooks/model/game/crush_ui.py
+++ notebooks/model/game/crush_ui.py
@@ -11,7 +11,7 @@ def display_via_javascript_script(element_id, board):
   return("""<script type="text/javascript">display_board("%s",[%s])</script>""" % (element_id, ','.join(a),))
   #return("<b>HelloWorld</b>")
 
-def display_via_javascript_callback(board):
+def render_to_json(board):
   return([ [ c for c in board[h].tolist() ] for h in range(0,board.shape[0]) ])
   
   #a = [ "[%s]" % (','.join([ "%d" % (c,) for c in board[h].tolist() ]),) for h in range(0, board.shape[0]) ]
@@ -20,12 +20,13 @@ def display_via_javascript_callback(board):
   #for h in range(0, c.shape[0]):
   #  for v in range(0, c.shape[1]):
 
-def display_gameplay(element_id, boards, timing):
-  arr = []
+def display_gameplay(element_id, boards, scores, timing):
+  b_arr = []
   for board in boards:
-    a = [ "[%s]" % (','.join([ "%d" % (c,) for c in board[h].tolist() ]),) for h in range(0, board.shape[0]) ]
-    arr.append( "[%s]" % (','.join(a),) )
-  return("""<script type="text/javascript">display_gameplay("%s",[%s], %f)</script>""" % (element_id, ','.join(arr), timing))
+    b = [ "[%s]" % (','.join([ "%d" % (c,) for c in board[h].tolist() ]),) for h in range(0, board.shape[0]) ]
+    b_arr.append( "[%s]" % (','.join(b),) )
+  s_arr = [ "%d" % (s,) for s in scores ]
+  return("""<script type="text/javascript">display_gameplay("%s",[%s],[%s],%f)</script>""" % (element_id, ','.join(b_arr), ','.join(s_arr), timing))
 
 
 
@@ -44,19 +45,22 @@ function create_board(board_id, horizontal, vertical, n_colours) {
       }
       trs.push("<tr>"+tds.join('')+"</tr>");
     }
-    $(board_id).append("<table border=0>"+trs.join('')+"</table>");
+    $(board_id)
+      .empty()
+      .append("<div class='score' style='width:"+(horizontal*20)+"px;text-align:center'>0</div>")
+      .append("<table border='0' style='margin-top:0;'>"+trs.join('')+"</table>");
     $(board_id+' table')
       .off('click')
       .on('click', 'td', function(ev) {
         var cell = $(this);
-        console.log("Cell clicked : ", cell);
+        //console.log("Cell clicked : ", cell);
         var h=cell.attr('h') - 0;
         var v=cell.attr('v') - 0;
         console.log("Cell(h,v)=("+h+","+v+")");
 
         // https://github.com/fluxtream/fluxtream-ipy/blob/master/Communication%20between%20kernel%20and%20javascript%20in%20iPython%202.0.ipynb
         function handle_python_output(msg) {
-          console.log(msg);
+          //console.log(msg);
           if( msg.msg_type == "error" ) {
             console.log("Javascript received Python error : ", msg.content);
           }
@@ -67,16 +71,16 @@ function create_board(board_id, horizontal, vertical, n_colours) {
             //console.log("ie : ", res_json);
             var res=JSON.parse( res_json ); 
             //console.log("res = ", res);
-            display_board(board_id, res.arr);
+            display_board(board_id, res.arr, undefined, res.score_inc);
           }
         }
         
         var cmd=[
-          'board, score, n_cols=crush.after_move(board, '+h+','+v+', '+n_colours+')',
-          'arr=crush_ui.display_via_javascript_callback(board)',
-          'dict(arr=arr,score=score,n_cols=n_cols)'
+          'board, score_inc, n_cols=crush.after_move(board, '+h+','+v+', '+n_colours+')',
+          'arr=crush_ui.render_to_json(board)',
+          'dict(arr=arr,score_inc=score_inc,n_cols=n_cols)'
         ].join(';');
-        console.log(cmd);
+        //console.log(cmd);
         
         kernel.execute(cmd, {iopub: {output: handle_python_output}}, {silent:false});
       });
@@ -84,9 +88,13 @@ function create_board(board_id, horizontal, vertical, n_colours) {
   //$(board_id).append("<b>Hello</b>");
   //kernel.execute(command);
 }
-function display_board(board_id, matrix, score) {
-  console.log("display_board("+board_id+","+JSON.stringify(matrix)+")");
+function display_board(board_id, matrix, score, score_inc) {
+  console.log("display_board("+board_id+","+JSON.stringify(matrix).substr(0,37)+"... ,"++score+","+score_inc+")");
   var col=['#fff','#00f','#0f0','#f00','#666','#aaa'];
+  if( typeof score == 'undefined' ) {
+    score = $(board_id+' .score').text() |0;
+    score += score_inc;
+  }
   $(board_id+' .score').text(score | 0);
   matrix.forEach(function(row,h) {
       row.forEach(function(c,v) {
@@ -96,11 +104,11 @@ function display_board(board_id, matrix, score) {
         });
     });
 }
-function display_gameplay(board_id, boards, timing) {
+function display_gameplay(board_id, boards, scores, timing) {
   function display_gameplay_step(board_i) {
     if(board_i<boards.length) {
-      console.log(boards[board_i]);
-      display_board(board_id, boards[board_i], 0); // score == TODO
+      //console.log(boards[board_i]);
+      display_board(board_id, boards[board_i], scores[board_i]);
       setTimeout( function() { display_gameplay_step(board_i+1); }, timing*1000);
     }
   }
@@ -132,5 +140,5 @@ if __name__ == "__main__":
   n_colours = 5
   b = crush.new_board(10,14,n_colours) # Same as portrait phone  1 screen~1k,  high-score~14k
   #print( display_via_javascript_script("#board_id", b) )
-  print( display_via_javascript_callback(b) )
+  print( render_to_json(b) )
   
