f165e79175308999565036fde24bcd31e73b05dd
Martin Andrews <GitHub@mdda.net>
2016-06-22 23:30:48 +0800

    Added to USB install script (except for OVA-sync)

--- 9-copy-to-thumbdrive.bash
+++ 9-copy-to-thumbdrive.bash
@@ -7,17 +7,15 @@ if [ -z "$target" ]; then
 fi
 
 echo "Target: '${target}/'"
-#exit 1
 
 most_recent_ova=`ls ./vm-images/*.ova | tail -1`
 if [ -z "$most_recent_ova" ]; then
   echo "Need to have created a .ova file first"
   exit 1
 fi
+#exit 1
 
 # target ~ /run/media/andrewsm/591F-4AF0
-
-rsync -avz --progress ${most_recent_ova} ${target}/
 
 # Windows and OSX binaries
 rsync -avz --progress ./vm-images/VirtualBox-5* ${target}/
@@ -25,4 +23,17 @@ rsync -avz --progress ./vm-images/VirtualBox-5* ${target}/
 # Linux binaries (Fedora, and some Ubuntus)
 rsync -avz --progress ./vm-images/virtualbox-5* ${target}/
 
+mkdir -p ${target}/presentation
+rsync -avz --progress ./presentation/reveal.js-2.6.2/* ${target}/presentation/
+
 ls -l ${target}/
+
+rm ${target}/*.ova
+
+exit 1
+
+
+rsync -avz --progress ${most_recent_ova} ${target}/
+
+ls -l ${target}/
+
