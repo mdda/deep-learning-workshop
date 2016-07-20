93c5800b3eb0b341832e072a9086decfc199e72a
Martin Andrews <GitHub@mdda.net>
2016-07-21 00:48:57 +0800

    Some small update logic

--- notebooks/model/updates.py
+++ notebooks/model/updates.py
@@ -1,3 +1,6 @@
 # update structure, and potentially code - pulled in by U-for-Upgrade.ipynb
 
-print("Hello Updates")
+if dt_tm<'2016-07-20_22-00':
+  print("Update process doesn't exist for this VM")
+else:
+  print("No updates required for workshop")
