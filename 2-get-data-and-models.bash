1a9aabbf9d1d69992b072a142ed33406d6a70f09
Martin Andrews <GitHub@mdda.net>
2017-05-05 23:57:01 +0800

    Redo downloader, since source has changed

--- 2-get-data-and-models.bash
+++ 2-get-data-and-models.bash
@@ -178,12 +178,13 @@ fi
 
 if [ ! -e "en.wikipedia.2010.100K.txt" ]; then
   # Retrieve wikipedia dump
-  wget http://corpora2.informatik.uni-leipzig.de/downloads/eng_wikipedia_2010_100K-text.tar.gz
-  if [ ! -e "eng_wikipedia_2010_100K-text.tar.gz" ]; then
+  wget http://corpora2.informatik.uni-leipzig.de/downloads/eng_wikipedia_2010_100K.tar.gz
+  if [ ! -e "eng_wikipedia_2010_100K.tar.gz" ]; then
     # Fall-back source of the same data
     wget ${RCL_BASE}/RNN/eng_wikipedia_2010_100K-text.tar.gz
+    mv eng_wikipedia_2010_100K-text.tar.gz eng_wikipedia_2010_100K.tar.gz 
   fi
-  tar -xzf eng_wikipedia_2010_100K-text.tar.gz
+  tar -xzf eng_wikipedia_2010_100K.tar.gz 
   mv eng_wikipedia_2010_100K-sentences.txt en.wikipedia.2010.100K.txt
   rm eng_wikipedia*
 fi
