0cdfb22e60c261b5c8beb2303c833ecd460cf5b9
Martin Andrews <GitHub@mdda.net>
2018-03-25 00:54:04 +0800

    Better ./clean creation

--- 3-make-clean-copy.bash
+++ 3-make-clean-copy.bash
@@ -32,13 +32,21 @@ rsync -avz --progress \
 rsync -avz --progress \
      ./notebooks/images ./clean/notebooks/
 
+# Link up the images directories
+ln -s ../../images ./clean/notebooks/2-CNN/5-TransferLearning/
+
+rsync -avz --progress \
+     ./presentation ./clean/
+
+# Link the presentation image directory into the tree too (already there)
+#ln -s ../../presentation/reveal.js-2.6.2/img ./clean/notebooks/images/presentation
+
+
 # Temporary clean-out
 rm ./clean/notebooks/2-CNN/8-Speech/SpeechAnalysis_*
 
-# Link up the images directory
-ln -s ./clean/images ./clean/notebooks/2-CNN/5-TransferLearning/
 
 # Copy over the locally installed keras zoo model parameters
-mkdir -p ./clean/dot_keras/models
+mkdir -p ./clean/.keras/models
 rsync -avz --progress ~/.keras/models/nasnet_mobile.h5 ./clean/.keras/models/
 rsync -avz --progress ~/.keras/models/nasnet_mobile_no_top.h5 ./clean/.keras/models/
