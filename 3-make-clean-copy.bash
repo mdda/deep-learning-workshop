410f1c1921c79113bb0536d35dde8b54cfbbface
Martin Andrews <GitHub@mdda.net>
2018-03-25 00:32:51 +0800

    More cleaning/moving

--- 3-make-clean-copy.bash
+++ 3-make-clean-copy.bash
@@ -29,6 +29,16 @@ rsync -avz --progress \
      --exclude="syntaxnet/" \
      ./notebooks/models ./clean/notebooks/
 
+rsync -avz --progress \
+     ./notebooks/images ./clean/notebooks/
 
+# Temporary clean-out
+rm ./clean/notebooks/2-CNN/8-Speech/SpeechAnalysis_*
+
+# Link up the images directory
+ln -s ./clean/images ./clean/notebooks/2-CNN/5-TransferLearning/
+
+# Copy over the locally installed keras zoo model parameters
 mkdir -p ./clean/dot_keras/models
+rsync -avz --progress ~/.keras/models/nasnet_mobile.h5 ./clean/.keras/models/
 rsync -avz --progress ~/.keras/models/nasnet_mobile_no_top.h5 ./clean/.keras/models/
