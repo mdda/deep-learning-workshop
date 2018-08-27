8fa2c65487ed5743f0b6fdbe04ac1cb465cade08
Martin Andrews <GitHub@mdda.net>
2018-08-27 22:32:28 +0800

    Set lr_initial on command line too

--- notebooks/work-in-progress/pay-attention-to-training-set/train_xception.py
+++ notebooks/work-in-progress/pay-attention-to-training-set/train_xception.py
@@ -19,6 +19,7 @@ parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
 parser.add_argument("--dataset_root", default='tiny-imagenet-200', type=str, help="directory with tiny ImageNet inside")
 parser.add_argument("--checkpoint",   default=None, type=str, help="model checkpoint path to restart training")
 parser.add_argument("--epoch",        default=0, type=int, help="model checkpoint epoch")
+parser.add_argument("--lr_initial",   default=0.01, type=float, help="initial lr (will be stepped down)")
 
 args = parser.parse_args()
 
@@ -100,8 +101,8 @@ model_base.last_linear = torch.nn.Linear(2048, num_classes).to(device)
 
 
 
-optimizer = torch.optim.SGD(model_base.parameters(), lr=0.01, momentum=0.9, )  # weight_decay=0.0001
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.3, last_epoch=(args.epoch-1))
+optimizer = torch.optim.SGD(model_base.parameters(), lr=args.lr_initial, momentum=0.9, )  # weight_decay=0.0001
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.3, initial_lr=args.lr_initial, last_epoch=(args.epoch-1))
 
 ce_loss = torch.nn.CrossEntropyLoss()
 
