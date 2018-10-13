3b5e4211f3917ee8cfa7e099f90e804882ba0144
Martin Andrews <GitHub@mdda.net>
2018-10-14 03:49:07 +0800

    Start to read the datafile

--- notebooks/work-in-progress/2018-10_ZeroShotRelationships/train_on_onestep.py
+++ notebooks/work-in-progress/2018-10_ZeroShotRelationships/train_on_onestep.py
@@ -1,4 +1,5 @@
 import os, sys
+
 import argparse
 import random
 
@@ -12,14 +13,71 @@ from model_pytorch import TransformerModel, load_openai_pretrained_model, DEFAUL
 from text_utils import TextEncoder
 from utils import ResultLogger
 
+import csv
+
 pretrained_model_path = os.path.join('.', 'orig', 'finetune-transformer-lm', 'model')
 
+# So, let's read in a text file
+relation_splits_path = os.path.join('.', 'orig', 'omerlevy-bidaf_no_answer-2e9868b224e4', 'relation_splits', )
 
 
-# So, let's read in the 
+#   840000  31087632 191519344 orig/omerlevy-bidaf_no_answer-2e9868b224e4/relation_splits/train.1
+#      600     21854    136415 orig/omerlevy-bidaf_no_answer-2e9868b224e4/relation_splits/dev.1
+#    12000    427110   2688895 orig/omerlevy-bidaf_no_answer-2e9868b224e4/relation_splits/test.1
+#   852600  31536596 194344654 total
+
+def yield_relations(relation_phase='train', relation_split=1, only_positive=True):
+  relation_file=os.path.join( relation_splits_path, "%s.%d" % (relation_phase, relation_split))
+  len_max_txt=0
+  with open(relation_file, 'r') as fp:
+    reader = csv.reader(fp, delimiter='\t')
+    for i, each in enumerate(reader):
+      rel, ques_xxx, ques_arg, sent = each[:4]
+      ques = ques_xxx.replace('XXX', ques_arg)
+        
+      indices = []
+      if len(each) > 4:
+        ans_list = each[4:]
+        # These are offsets in characters
+        indices = [(sent.index(ans), sent.index(ans) + len(ans)) for ans in ans_list]
+        #starts, ends = zip(*indices)
+        #ans_start = min(starts)
+        #ans_end = max(ends)
+        #ans = sent[ans_start:ans_end]
+        #qa['answers'].append({'text': ans, 'answer_start': ans_start})
+      else:
+        # Negative example
+        pass
+            
+      #print(rel, indices)
+      enc_ques = text_encoder.encode(ques, verbose=False)
+      #print( len(ques), len(enc_ques) )
+      #if len(ques) != len(enc_ques):
+      #  print( ques )
+      #print( enc_ques )
+    
+      if i % 10000 == 0:
+        print("Line %d" % (i,))
+    
+      if only_positive and len(indices)==0:
+        continue
+      
+      len_txt = len(ques) + len(sent) + 3
+      
+      if len_max_txt<len_txt:
+        len_max_txt=len_txt
+        print( ques, sent )
+        print(len_max_txt)
+        
+      #if i>1000: break
+      
+  print(len_max_txt)
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    
     parser.add_argument('--desc', type=str, default='default', help="Description")
     #parser.add_argument('--dataset', type=str)
     parser.add_argument('--log_dir', type=str, default='log/')
@@ -83,6 +141,7 @@ if __name__ == '__main__':
     print("device", device, "n_gpu", n_gpu)
 
     logger = ResultLogger(path=os.path.join(log_dir, '{}.jsonl'.format(desc)), **args.__dict__)
+    
     text_encoder = TextEncoder(args.encoder_path, args.bpe_path)
     encoder = text_encoder.encoder
     n_vocab = len(text_encoder.encoder)
@@ -93,27 +152,35 @@ if __name__ == '__main__':
     # (teX1, teX2, teX3)) = encode_dataset(*rocstories(data_dir, n_valid=args.n_valid),
     #                                      encoder=text_encoder)
     
-    encoder['_start_'] = len(encoder)
-    encoder['_delimiter_'] = len(encoder)
-    encoder['_classify_'] = len(encoder)
+    regular_tokens = n_vocab
+    encoder['_start_'] = len(encoder)      # Last number (increments)
+    encoder['_delimiter_'] = len(encoder)  # Last number (increments)
+    encoder['_classify_'] = len(encoder)   # Last number (increments)
+    
     clf_token = encoder['_classify_']
+    #n_special = 3  
+    n_special = len(encoder) - regular_tokens  # Number of extra tokens
     
-    n_special = 3
-    max_len = n_ctx // 2 - 2
+    yield_relations()
+    exit(0)
     
-    n_ctx = min(max(
-        [len(x1[:max_len]) + max(len(x2[:max_len]),
-                                 len(x3[:max_len])) for x1, x2, x3 in zip(trX1, trX2, trX3)]
-        + [len(x1[:max_len]) + max(len(x2[:max_len]),
-                                   len(x3[:max_len])) for x1, x2, x3 in zip(vaX1, vaX2, vaX3)]
-        + [len(x1[:max_len]) + max(len(x2[:max_len]),
-                                   len(x3[:max_len])) for x1, x2, x3 in zip(teX1, teX2, teX3)]
-        ) + 3, n_ctx)
+    #max_len = n_ctx // 2 - 2
+    
+    #n_ctx = min(max(
+    #    [len(x1[:max_len]) + max(len(x2[:max_len]),
+    #                             len(x3[:max_len])) for x1, x2, x3 in zip(trX1, trX2, trX3)]
+    #    + [len(x1[:max_len]) + max(len(x2[:max_len]),
+    #                               len(x3[:max_len])) for x1, x2, x3 in zip(vaX1, vaX2, vaX3)]
+    #    + [len(x1[:max_len]) + max(len(x2[:max_len]),
+    #                               len(x3[:max_len])) for x1, x2, x3 in zip(teX1, teX2, teX3)]
+    #    ) + 3, n_ctx)
+    
+    n_ctx = 100 # max length of encoded strings...
         
     vocab = n_vocab + n_special + n_ctx
     
-    trX, trM = transform_roc(trX1, trX2, trX3)
-    vaX, vaM = transform_roc(vaX1, vaX2, vaX3)
+    #trX, trM = transform_roc(trX1, trX2, trX3)
+    #vaX, vaM = transform_roc(vaX1, vaX2, vaX3)
     
     #if submit:
     #    teX, teM = transform_roc(teX1, teX2, teX3)
