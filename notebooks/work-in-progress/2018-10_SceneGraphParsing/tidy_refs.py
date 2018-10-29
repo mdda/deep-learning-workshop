a3b1b49ca44793e7b3141a5dbdec1728d7ee08d8
Martin Andrews <GitHub@mdda.net>
2018-10-30 01:59:33 +0800

    --testnodes function for object/attr/relationship consistency

--- notebooks/work-in-progress/2018-10_SceneGraphParsing/tidy_refs.py
+++ notebooks/work-in-progress/2018-10_SceneGraphParsing/tidy_refs.py
@@ -16,6 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--input",  help="Required Processed input file", type=str, default=output_path+"pre_coco_train.json.rows")
 parser.add_argument("--output", help="Processed file output file",    type=str, default=output_path+"pre_coco_train_clean.json.rows")
 parser.add_argument("--multiwords",  help="Create ./multiwords.json", action='store_true')
+parser.add_argument("--testnodes",  help="Ensure relationship and attribute objects are present", action='store_true')
 #parser.add_argument("--train",  help="Check if processed file required Training", action='store_true')
 args = parser.parse_args()
 
@@ -29,7 +30,7 @@ if args.multiwords:
   multiwords=dict(r=defaultdict(int),a=defaultdict(int),o=defaultdict(int)) # Empty to start (relationships, attributes, objects)
 else:
   if os.path.isfile(multiwords_path):
-    multiwords = json.load(multiwords_path)
+    multiwords = json.load(open(multiwords_path, 'r'))
   else:
     print("Need to create --multiwords")
     exit(0)
@@ -42,14 +43,14 @@ for row_num, refs_json in enumerate(refs_file):
   phrases, objects, attributes, relationships  = refs_arr
   phrase=phrases[0]
 
-  phrase=phrase.replace('.', ' ').replace(',', ' ').replace('  ', ' ').strip()
+  phrase=phrase.lower().replace('.', ' ').replace(',', ' ').replace('  ', ' ').strip()
 
   #for relationship in relationships:
   #  relatfor r in relationship:
   
   #if row_num>1000: break
   
-  print(row_num, phrase)
+  #print(row_num, phrase)
   
   if args.multiwords:
     for relationship in relationships:
@@ -69,6 +70,27 @@ for row_num, refs_json in enumerate(refs_file):
         print("Multiword object : '%s'" % (obj,))
         multiwords['o'][obj.strip()] += 1
     continue
+    
+  obj_set = set([ obj.strip() for obj in objects ])
+
+  if args.testnodes:
+    for relationship in relationships:
+      a, action, b = [ r.strip() for r in relationship]
+      if a not in obj_set:
+        print("Didn't find relationship object-a '%s' in '%s' for '%s'" % (a, "|".join(list(obj_set)), phrase,))
+      if b not in obj_set:
+        print("Didn't find relationship object-b '%s' in '%s' for '%s'" % (b, "|".join(list(obj_set)), phrase,))
+      
+    for attribute in attributes:
+      a = attribute[0]
+      if a not in obj_set:
+        print("Didn't find attributed object'%s' in '%s' for '%s'" % (a, "|".join(list(obj_set)), phrase,))
+    
+        
+    
+    
+    
+    
     
     
 
